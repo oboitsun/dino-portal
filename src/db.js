d570b610d43597a6cd33d64b19bf83cc2f2013f1
oboitsun <thewip3out@gmail.com>
2022-02-14 10:37:17 +0200

    changed content in login header

--- src/db.js
+++ src/db.js
@@ -1,6 +1,6 @@
 export const links = [
   //   { text: 'home', href: 'https://www.icdinos.io' },
-  { text: 'back home', href: 'https://dinos-five.vercel.app' },
+  { text: 'Leave Dino Portal', href: 'https://dinos-five.vercel.app' },
   //   { text: 'about', href: 'https://www.icdinos.io#about-us' },
   //   { text: 'team', href: 'https://www.icdinos.io#team' },
   //   { text: 'roadmap', href: 'https://www.icdinos.io#roadmap' },
