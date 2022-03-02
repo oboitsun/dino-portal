c0ad37b88a394a193d642a264f8a86b68d31997b
oboitsun <thewip3out@gmail.com>
2022-03-02 11:36:40 +0200

    new backs and eggs

---
+++ src/pages/Home/backs.js
@@ -0,0 +1,9 @@
+export const backs = [
+  'assets/backs/1.jpg',
+  'assets/backs/2.jpg',
+  'assets/backs/3.jpg',
+  'assets/backs/4.jpg',
+  'assets/backs/5.jpg',
+  'assets/backs/6.jpg',
+  'assets/backs/7.jpg',
+];
