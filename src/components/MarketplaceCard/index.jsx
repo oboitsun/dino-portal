2ce08a3482354b116add4a986b232858d5d0d525
oboitsun <thewip3out@gmail.com>
2022-05-17 10:58:05 +0300

    new marketplace pics

--- src/components/MarketplaceCard/index.jsx
+++ src/components/MarketplaceCard/index.jsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import './marketplace-card.scss';
-import RarityButton from './RarityButton';
+import React from "react";
+import "./marketplace-card.scss";
+import RarityButton from "./RarityButton";
 export default function MarketplaceCard({ card }) {
   const rarityColors = {
-    common: '#6FCD93',
-    uncommon: '#61C8F7',
-    rare: '#D88535',
-    ultraRare: '#F0CB64',
+    common: "#6FCD93",
+    uncommon: "#61C8F7",
+    rare: "#D88535",
+    ultraRare: "#F0CB64",
   };
   return (
-    <div className="w-full flex flex-col ">
+    <div className="MarketplaceCard w-full flex flex-col ">
       <div className="w-full relative">
         <div
           style={{ background: rarityColors[card.rarity] }}
@@ -20,12 +20,8 @@ export default function MarketplaceCard({ card }) {
         <img className="w-full" src={card.imgSrc} alt="nft-card" />
       </div>
       <div className="w-full flex items-center text-2xl py-1 justify-between px-1">
-        <p className="text-border text-white text-xs lg:text-base">
-          {card.name}
-        </p>
-        <p className="text-yellow text-border text-xs lg:text-base">
-          {card.price} ICP
-        </p>
+        <p className="text-border text-white text-xs lg:text-sm">{card.name}</p>
+        <p className="text-yellow text-border text-xs lg:text-base">{card.price} ICP</p>
       </div>
       <a className="w-full block" href="/">
         <img className="w-full" src="assets/buy-now.svg" alt="buy now" />
